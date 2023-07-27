2ddadadf3d56e553cf04f296961801f45955b7cb
Deimer M <deimer.morales@edunext.co>
2023-07-26 22:51:13 -0500

    feat: adding scaffold for new payment processor

---
+++ README.rst
@@ -0,0 +1,11 @@
+Ecommerce plugin paygate Payment Processor backend for Open edX ecommerce
+=========================================================
+
+This application provides a custom `Open edX ecommerce <https://github.com/edx/ecommerce/>`
+payment processor backend for the
+`HyperPay payment processor <https://wordpresshyperpay.docs.oppwa.com/tutorials/integration-guide>`_.
+
+Installation and usage
+######################
+
+* Install this repository inside the ecommerce virtualenv environment using `pip`.
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+Setup file for the ecommerce-plugin-paygate Open edX ecommerce payment processor backend plugin.
+"""
+
+from pathlib import Path
+
+from setuptools import setup
+
+README = open(Path(__file__).parent / 'README.rst').read()
+CHANGELOG = open(Path(__file__).parent / 'CHANGELOG.rst').read()
+
+
+setup(
+    name='ecommerce-plugin-paygate',
+    description='Ecommerce plugin paygate payment processor backend plugin',
+    version='0.1.0',
+    author='eduNEXT',
+    author_email='contact@edunext.co',
+    long_description=f'{README}\n\n{CHANGELOG}',
+    long_description_content_type='text/x-rst',
+    url='https://github.com/edunext/ecommerce-plugin-paygate',
+    include_package_data=True,
+    zip_safe=False,
+    keywords='Django openedx openedx-plugin ecommerce paygate',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+        'Django~=3.2',
+    ],
+    packages=[
+        'paygate',
+    ],
+    entry_points={
+        'ecommerce': [
+            'paygate = paygate.apps:PayGatePayConfig',
+        ],
+    },
+)
