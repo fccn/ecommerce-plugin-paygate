359c5bba56f47bc3fa5395d686148099e74e6fbe
Ivo Branco <ivo.branco@fccn.pt>
2023-09-22 10:43:57 +0100

    First milestone of paygate integration.

--- README.rst
+++ README.rst
@@ -3,16 +3,30 @@ Paygate payment processor for Open edX
 ==================================================================================
 
 
-This is a placeholder payment processor for the `Ecommerce <https://edx-ecommerce.readthedocs.io/en/latest/>`__ application from `Open edX <https://open.edx.org/>`__.
+This is a placeholder payment processor for the `Ecommerce <https://edx-ecommerce.readthedocs.io/en/latest/>`__ 
+application from `Open edX <https://open.edx.org/>`__.
 
-This plugin was created by `eduNEXT <https://www.edunext.co/>`__ to work specifically with the `Tutor <https://docs.tutor.overhang.io/>`__ Open edX distribution. The following installation instructions should work wit the `tutor-ecommerce plugin <https://github.com/overhangio/tutor-ecommerce>`__, but there is no reason to believe that it shouldn't also work with the Open edX native installation.
+This plugin was bootstraped by `eduNEXT <https://www.edunext.co/>`__ and 
+implemented by the `NAU FCCN team <https://www.fccn.pt>`__ to work specifically with 
+the `Tutor <https://docs.tutor.overhang.io/>`__ Open edX distribution. 
+The following installation instructions should work wit the 
+`tutor-ecommerce plugin <https://github.com/overhangio/tutor-ecommerce>`__, 
+but there is no reason to believe that it shouldn't also work with the Open edX native installation.
 
 This guide is based in the `openedx-saferpay <https://github.com/epfl-cede/openedx-saferpay/tree/master>`__ README.
+
+Other projects that this project receive inspiration:
+- https://github.com/epfl-cede/openedx-saferpay
+- https://github.com/nelc/ecommerce-hyperpay
+- https://github.com/eduNEXT/ecommerce-extensions/tree/ee25624582c8e42b7eb998fda54358a7025c2dce/ecommerce_extensions/payment/processors
+- https://github.com/open-craft/ecommerce-paytabs
+
+Tutor
+===============
 
 Install the following version of tutor-ecommerce as it contains special code to avoid the usage of discovery service for development purposes:
 
     pip install git+https://github.com/eduNEXT/tutor-ecommerce@nau/v14.0.1#egg=tutor-ecommerce==v14.0.1
-
 
 Make sure that the ecommerce plugin is enabled::
 
@@ -39,19 +53,113 @@ Save your PayGate credentials to paygate.yml::
 
     $ cat paygate.yml
     paygate:
-        payment_page_url: your-payment-page-url
-        some_value: some-extra-value
+        access_token: PwdX_XXXX_YYYY
+        merchant_code: NAU
+        api_checkout_url: https://lab.optimistic.blue/paygateWS/api/CheckOut
+        api_basic_auth_user: username
+        api_basic_auth_pass: password
+        cancel_checkout_path: /checkout/cancel-checkout/
+        error_path: /checkout/error/
     $ tutor config save --set "ECOMMERCE_PAYMENT_PROCESSORS=$(cat paygate.yml)"
+
+    The `cancel_checkout_path` and `error_path` are optional.
 
 Run initialization scripts::
 
     tutor local quickstart
 
-Enable the Saferpay payment backend::
+Enable the Paygate payment backend::
 
     tutor local run ecommerce ./manage.py waffle_switch --create payment_processor_active_paygate on
 
 All payments will then proceed through the Paygate payment processor.
+
+
+Devstack
+===============
+
+Next, it is described the instructions to run the development environmen using the Open edX `Devstack <https://github.com/openedx/devstack>`__.
+
+If you are using some internal FCCN IaaS VM, please use FCCN forked `Devstack <https://github.com/fccn/devstack>`__ for that Open edX release.
+
+
+Troubleshooting
+-----------------
+
+If the service `frontend-app-payment` didn't start and has this on the docker logs::
+    sh: 1: fedx-scripts: not found
+
+The reason was because the `npm install` were raising some error::
+    Invalid tag name ">=^16.0.0" of package "react@>=^16.0.0": Tags may not have any characters that encodeURIComponent encodes.
+
+To fix run this inside the container::
+    npm install --legacy-peer-deps
+
+Configuration
+===============
+
+To develop using the devstack edit the `ecommerce/settings/private.py` file add change to::
+    PAYMENT_PROCESSOR_CONFIG = {
+        "edx": {
+            "paygate": {
+                "access_token": 'PwdX_XXXX_YYYY',
+                "merchant_code": "NAU",
+                "api_checkout_url": 'https://lab.optimistic.blue/paygateWS/api/CheckOut',
+                "api_basic_auth_user": "NAU",
+                "api_basic_auth_pass": "APassword",
+                "payment_types": ["VISA", "MASTERCARD", "AMEX", "PAYPAL", "MBWAY", "REFMB", "DUC"]
+            }
+        }
+    }
+    PAYMENT_PROCESSORS = ("ecommerce_plugin_paygate.processors.PayGate",)
+    EXTRA_PAYMENT_PROCESSOR_URLS = {"paygate": "ecommerce_plugin_paygate.urls"}
+    OSCAR_DEFAULT_CURRENCY = 'EUR'
+    LANGUAGE_CODE = "pt"
+    from django.utils.translation import ugettext_lazy as _
+    LANGUAGES = (
+        ('pt-pt', _('Português')),
+        ('en', _('English')),
+    )
+    LOGO_URL = "https://lms.nau.edu.pt/static/nau-basic/images/nau_azul.svg"
+    
+    # Use custom tax strategy
+    NAU_EXTENSION_OSCAR_STRATEGY_CLASS = "ecommerce_plugin_paygate.strategy.DefaultStrategy"
+    
+    # Configure tax as 23% used in Portugal
+    NAU_EXTENSION_TAX_RATE = "0.298701299" # = 0.23/0.77
+
+
+Clone the repository https://github.com/fccn/ecommerce-plugin-paygate to the `src` folder of the devstack (the parent `src` folder of the devstack folder)
+
+Install this plugin inside the ecommerce container::
+    make dev.shell.ecommerce
+    pip install -e /edx/src/ecommerce-plugin-paygate
+
+Restart ecommerce application::
+    make dev.restart-container.ecommerce
+
+On the Open edX Ecommerce user interface it is need to activate the PayGate payment processor.
+To activate the `paygate` add switch with the name `payment_processor_active_paygate` on
+http://localhost:18130/admin/waffle/switch/.
+
+On ecommerce Django admin site configuration,
+http://localhost:18130/admin/core/siteconfiguration/1/change/
+- disable Microfrontend for Basket Page
+- replace payment processors from `cybersource,paypal` to `paygate`
+
+Callbacks
+===============
+
+There are different callbacks that the Paygate uses.
+The success, cancel and failure callbacks are used to redirect the user after he has payed with success,
+has cancel the payment inside the Paygate user interface or some error has been raised.
+
+Additionally, there is also a server-to-server callback, the Paygate calls the Ecommerce informing
+that some payment reference has been payed.
+
+Example of the server callback, change the `payment_ref` with your basked identification::
+
+    curl -d '{"statusCode":"C", "success":"true", "MerchantCode":"NAUFCCN", "returnCode":"ABCDEFGHI", "shortMsg":"Opera%C3%A7%C3%A3o%20bem%20sucedida", "name": "edx", "is_paid": "true", "paymentValue": "1.00", "payment_ref": "EDX-100019"}' -H "Content-Type: application/json" -X POST http://localhost:18130/payment/paygate/callback/server/
 
 License
 =======
--- paygate/views.py
+++ paygate/views.py
@@ -1,107 +1,315 @@
 """
-  Paygate payment processing views in these views the callback pages will be implemented
+Paygate payment processing views in these views the callback pages will be implemented
 """
+import json
+import abc
 import logging
 
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseServerError
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from django.shortcuts import redirect
 from django.views.generic import View
-from oscar.apps.partner import strategy
-from oscar.core.loading import get_class, get_model
-from rest_framework import status
-
-from .processors import PayGate
-
 from ecommerce.extensions.checkout.mixins import EdxOrderPlacementMixin
 from ecommerce.extensions.checkout.utils import get_receipt_page_url
-from ecommerce.extensions.payment.exceptions import InvalidSignatureError
+from ecommerce.extensions.partner import strategy
+from oscar.apps.payment.exceptions import PaymentError
+from oscar.core.loading import get_class, get_model
+
+from .ip import *
+from .processors import PayGate
+
+# from oscar.apps.partner import strategy
 
 
 logger = logging.getLogger(__name__)
 
-Applicator = get_class('offer.applicator', 'Applicator')
-Basket = get_model('basket', 'Basket')
-OrderNumberGenerator = get_class('order.utils', 'OrderNumberGenerator')
+Applicator = get_class("offer.applicator", "Applicator")
+Basket = get_model("basket", "Basket")
+OrderNumberGenerator = get_class("order.utils", "OrderNumberGenerator")
+PaymentProcessorResponse = get_model("payment", "PaymentProcessorResponse")
 
 
+class PaygateCallbackBaseResponseView(
+    EdxOrderPlacementMixin, View, metaclass=abc.ABCMeta
+):
+    # Disable atomicity for the view. Otherwise, we'd be unable to commit to the database
+    # until the request had concluded; Django will refuse to commit when an atomic() block
+    # is active, since that would break atomicity. Without an order present in the database
+    # at the time fulfillment is attempted, asynchronous order fulfillment tasks will fail.
+    @method_decorator(transaction.non_atomic_requests)
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-class PaygatePaymentResponseView(EdxOrderPlacementMixin, View):
-  
-  @property
-  def payment_processor(self):
+    @property
+    def payment_processor(self):
+        """
+        An instance of Paygate payment processor.
+        """
+        return PayGate(self.request.site)
+
+    def _get_basket(self, basket_id):
+        if not basket_id:
+            return None
+        try:
+            basket_id = int(basket_id)
+            basket = Basket.objects.get(id=basket_id)
+            basket.strategy = strategy.Selector().strategy()
+            Applicator().apply(basket, basket.owner, self.request)
+            return basket
+        except (ValueError, ObjectDoesNotExist):
+            return None
+
+    def get_basket_and_record_response(self, request):
+        """
+        Get the basket object and save the processor response.
+
+        Returns:
+            Basket: the basket object that this callback references.
+            PaymentProcessorResponse: The auditing model used to store the Paygate processor
+                response.
+        """
+        paygate_response = (
+            # if HTTP method POST then the payload is a JSON
+            json.loads(request.body)
+            if request.method == "POST"
+            else request.GET.dict()
+        )
+        logger.info(paygate_response)
+
+        # ppr = get_object_or_404(PaymentProcessorResponse, id=ppr_id)
+
+        # Get Basket from sent from Checkout Paygate API has the call back server params.
+        transaction_id = paygate_response.get("payment_ref")
+        if transaction_id:
+            basket_id = OrderNumberGenerator().basket_id(transaction_id)
+            basket = self._get_basket(basket_id)
+
+            ppr = self.payment_processor.record_processor_response(
+                paygate_response,
+                transaction_id=transaction_id,
+                basket=basket,
+            )
+            return basket, ppr
+        else:
+            logger.error("Missing 'payment_ref' parameter from request")
+            return None, None
+
+
+class PaygateCallbackServerResponseView(PaygateCallbackBaseResponseView):
     """
-      An instance of Paygate payment processor.
+    A server-to-server notification that informs the ecommerce if the payment on the paygate
+    has been with success or not.
+    The decision is based on the payload of this call.
     """
-    return PayGate(self.request.site)
-  
-  # Disable atomicity for the view. Otherwise, we'd be unable to commit to the database
-  # until the request had concluded; Django will refuse to commit when an atomic() block
-  # is active, since that would break atomicity. Without an order present in the database
-  # at the time fulfillment is attempted, asynchronous order fulfillment tasks will fail.
-  @method_decorator(transaction.non_atomic_requests)
-  @method_decorator(csrf_exempt)
-  def dispatch(self, request, *args, **kwargs):
-      return super().dispatch(request, *args, **kwargs)
-  
-  def _get_basket(self, basket_id):
-    if not basket_id:
-        return None
-    try:
-      basket_id = int(basket_id)
-      basket = Basket.objects.get(id=basket_id)
-      basket.strategy = strategy.Default()
-      Applicator().apply(basket, basket.owner, self.request)
-      return basket
-    except (ValueError, ObjectDoesNotExist):
-        return None
-  
-  def post(self, request, *args, **kwargs):
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handle the callback request if it is done via HTTP GET method.
+
+        It will send an HTTP 405 response indicating that the GET is not allowed and the client
+        should send it using a POST HTTP method.
+        """
+        return HttpResponseNotAllowed(["POST"])
+
+    def post(self, request, *args, **kwargs):
+        """
+        This function will handle the callback request in case it is done via HTTP POST method
+
+        Implementation of the server-to-server callback from Paygate to Ecommerce.
+        This method should be protected with an `callback_server_allowed_networks` paygate
+        configuration with a list of allowed networks to send this POST.
+
+        To view the payload of this POST, please see the `ServerCallbackExample` Schema input of
+        the Paygate Swagger.
+
+        In case of some exception/error this method will send only the HTTP response status code
+        without an user interface, because this method should be called from the Paygate.
+        """
+
+        allowed_networks = self.payment_processor.callback_server_allowed_networks
+        if not allowed_networks:
+            logger.warning(
+                "Paygate possible security risk missing 'callback_server_allowed_networks' configuration!"
+            )
+        if allowed_networks and not allowed_client_ip(
+            get_client_ip(request),
+            allowed_networks,
+        ):
+            return HttpResponse("Unauthorized invalid allowed ip address", status=401)
+        # else: the client ip is allowed
+
+        (basket, payment_processor_response) = self.get_basket_and_record_response(
+            request
+        )
+
+        payed_with_success = (
+            bool(payment_processor_response.response.get("success", False))
+            and payment_processor_response.response.get("statusCode", "") == "C"
+        )
+        if not payed_with_success:
+            logger.warning(
+                "Paygate server callback without success and correct statusCode of 'C'"
+            )
+            return HttpResponse("Incorrect success and status code", status=412)
+
+        try:
+            # Explicitly delimit operations which will be rolled back if an exception occurs.
+            with transaction.atomic():
+                # This method have to be invoked in order to handle a payment, this method could raise an PaymentError exception.
+                self.handle_payment(payment_processor_response.response, basket)
+        except PaymentError:
+            logger.exception(
+                "Paygate server callback error while handling payment with a payment error for basket [%d]",
+                basket.id,
+            )
+            return HttpResponseServerError(
+                "Error while handling payment - payment error"
+            )
+        except:  # pylint: disable=bare-except
+            logger.exception(
+                "Paygate server callback error while handling payment with another error for basket [%d]",
+                basket.id,
+            )
+            return HttpResponseServerError("Error while handling payment - other error")
+
+        # if the basket hasn't already contain an order, create one
+        if not hasattr(basket, "order"):
+            # the basket already contains an order.
+            # we could receive duplicated server callbacks.
+            logger.warning(
+                "Paygate server callback the basket already has an order for basket [%d]",
+                basket.id,
+            )
+        else:
+            # create an order for the basket
+            try:
+                order = self.create_order(request, basket)
+            except Exception:  # pylint: disable=broad-except
+                logger.exception(
+                    "Paygate server callback error while creating order for basket [%d]",
+                    basket.id,
+                )
+                return HttpResponseServerError("Error while creating order")
+
+            try:
+                self.handle_post_order(order)
+            except Exception:  # pylint: disable=broad-except
+                self.log_order_placement_exception(basket.order_number, basket.id)
+
+        return HttpResponse("Received server callback with success")
+
+
+class PaygateCallbackSuccessResponseView(PaygateCallbackBaseResponseView):
     """
-    This function will handle the callback request in case it is done via HTTP POST method
+    This view is used by the Paygate frontend to redirect the user after he has payed with
+    success.
+    This callback should NOT be used to fullfill the order.
+
+    We should just check if we have received the callback server-to-server request. If yes we
+    should just inform the student that he will be enrolled to the course in a couple of moments.
+    If not we should inform the student that we are still waiting to receive the payment
+    notification.
     """
-    self.process_request(request)
-    return HttpResponse()
-  
-  def get(self, request, *args, **kwargs):
+
+    def get(self, request, *args, **kwargs):
+        """
+        This function will handle the callback request in case it is done via HTTP GET method
+        """
+        (basket, payment_processor_response) = self.get_basket_and_record_response(
+            request
+        )
+        if not basket:
+            logger.warning("Paygate no basket found on the callback success")
+            return redirect(self.payment_processor.failure_url)
+
+        receipt_url = get_receipt_page_url(
+            self.request.site.siteconfiguration,
+            order_number=basket.order_number,
+            disable_back_button=True,
+        )
+
+        if not hasattr(basket, "order"):
+            # Received the frontend success callback before received the server-to-server callback
+
+            payed_with_success = (
+                bool(payment_processor_response.response.get("is_paid", False))
+                and payment_processor_response.response.get("StatusCode", "") == "C"
+            )
+            if not payed_with_success:
+                logger.warning(
+                    "Paygate server callback without success and correct statusCode of 'C'"
+                )
+                return HttpResponse("Incorrect success and status code", status=412)
+
+            try:
+                # Explicitly delimit operations which will be rolled back if an exception occurs.
+                with transaction.atomic():
+                    # This method have to be invoked in order to handle a payment, this method could raise an PaymentError exception.
+                    self.handle_payment(payment_processor_response.response, basket)
+            except PaymentError:
+                return redirect(self.payment_processor.error_url)
+            except:  # pylint: disable=bare-except
+                logger.exception(
+                    "Attempts to handle payment for basket [%d] failed.", basket.id
+                )
+                return redirect(receipt_url)
+
+            try:
+                order = self.create_order(request, basket)
+            except Exception:  # pylint: disable=broad-except
+                return redirect(receipt_url)
+
+            try:
+                self.handle_post_order(order)
+            except Exception:  # pylint: disable=broad-except
+                self.log_order_placement_exception(basket.order_number, basket.id)
+
+            return redirect(receipt_url)
+        # else
+        #   basked already has an order, ok the Paygate already has successfully called the server
+        #      callback.
+
+        return redirect(receipt_url)
+
+
+class PaygateCallbackRedirectResponseView(PaygateCallbackBaseResponseView):
     """
-    This function will handle the callback request in case it is done via HTTP GET method
+    This is base view for callbacks that just redirect the user
     """
-    receipt_url= self.process_request(request)
-    return redirect(receipt_url)
-  
-  
-  def process_request(self, request,  *args, **kwargs):
-    # pylint: disable=unused-argument
+
+    def get(self, request, *args, **kwargs):
+        """
+        This function will handle the callback request in case it is done via HTTP GET method
+        """
+        self.get_basket_and_record_response(request)
+        return redirect(self.url_to_redirect())
+
+    @abc.abstractmethod
+    def url_to_redirect(self):
+        raise NotImplementedError
+
+
+class PaygateCallbackCancelResponseView(PaygateCallbackRedirectResponseView):
     """
-      For this example, we implement a get method that 
-      handles an incoming user that is returned to us after processing the payment
-      in this case, we assume that the order number of your basket and the transaction ID comes from the request of your payment processor.
+    This view is used by Paygate frontend to redirect the user after he has cancel the payment on
+    the Paygate user interface.
     """
-    paygate_response = request.POST.dict() if request.method == "POST" else request.GET.dict()
-    logger.info(paygate_response)
-    order_number = paygate_response.get('order_number')
-    transaction_id = paygate_response.get('transaction_id')
-    basket_id = OrderNumberGenerator().basket_id(order_number)
-    basket = self._get_basket(basket_id)
-    ppr = self.payment_processor.record_processor_response(
-        paygate_response,
-        transaction_id=transaction_id,
-        basket=basket,
-    )
-    # Explicitly delimit operations which will be rolled back if an exception occurs.
-    with transaction.atomic():
-      # This method have to be invoked in order to handle a payment, this method could raise an PaymentError exception.
-      self.handle_payment(paygate_response, basket)
-    
-    receipt_url = get_receipt_page_url(
-      order_number=basket.order_number,
-      site_configuration=basket.site.siteconfiguration,
-    )
-    
-    order = self.create_order(request, basket)
-    
-    return receipt_url
+
+    def url_to_redirect(self):
+        return self.payment_processor.cancel_url
+
+
+class PaygateCallbackFailureResponseView(PaygateCallbackRedirectResponseView):
+    """
+    This view is used by Paygate frontend to redirect the user when some error has been raised
+    inside the Paygate.
+    """
+
+    def url_to_redirect(self):
+        return self.payment_processor.failure_url
