2b08249292df562be55d0307bb7375ac1d2f038f
Ivo Branco <ivo.branco@fccn.pt>
2024-07-29 15:48:17 +0100

    fix: tests for upgrade for redwood
    Fix the tests and get receipt url for Redwood upgrade.
    
    fccn/nau-technical#221

--- paygate/utils.py
+++ paygate/utils.py
@@ -1,6 +1,10 @@
+import inspect
+
 from django.core.exceptions import ObjectDoesNotExist
 from oscar.core.loading import get_class, get_model
 
+from ecommerce.extensions.checkout.utils import \
+    get_receipt_page_url as ecommerce_get_receipt_page_url
 from ecommerce.extensions.partner import strategy
 
 Order = get_model("order", "Order")
@@ -39,3 +43,15 @@ def get_basket_from_payment_ref(payment_ref):
     """
     basket_id = OrderNumberGenerator().basket_id(payment_ref)
     return get_basket(basket_id)
+
+
+def get_receipt_page_url(request, order_number=None):
+    """
+    Returns the receipt page URL.
+    """
+    params = inspect.signature(ecommerce_get_receipt_page_url).parameters
+    if len(params) == 4:
+        # old version
+        return ecommerce_get_receipt_page_url(request.site.siteconfiguration, order_number=order_number, disable_back_button=True)
+    else:
+        return ecommerce_get_receipt_page_url(request, request.site.siteconfiguration, order_number=order_number, disable_back_button=True)
